b501756cf2257182ca68d40021bdd27cf5310aed
Venkat Narayanan Balachandran <79725511+VenkatNarayanan11@users.noreply.github.com>
2021-02-27 23:58:12 -0500

    Update MPC_lane_following.m

--- MPC_lane_following.m
+++ MPC_lane_following.m
@@ -9,14 +9,14 @@ tau = 0.5   % time constant for acceleration tracking in seconds.
  Cr = 33000 % cornering stiffness of rear tires in N/rad
  Ts = 0.1   % Sample time in seconds
 
-% Adaptive cruise control predictive model
+% Adaptive cruise control model
 A1 = [-1/tau 0;
        1 0]
 B1 = [1/tau; 0]
 C1 = [0 1]
 D1 = 0
 
-% Lane-keeping predicive model
+% Lane-keeping predictive model
 A2 = [(-2*(Cf + Cr)/m / Vx)   (-Vx-2*(Cf*Lf - Cr*Lr)/m /Vx);
       (-2*(Cf*Lf - Cr*Lr)/Iz/Vx)  (-2*(Cf*Lf^2 + Cr*Lr^2)/Iz/Vx)]
   
@@ -25,11 +25,11 @@ C2 = [1 0;
       0 1]
 D2 = [0;0]  
 
-% Combined path-following predictive control
+% Combined path-following predictive control model
 % The inputs are the longitudinal acceleration in m/s2 and 
-...steering angle in radians. 
+% steering angle in radians. 
 % The outputs are the longitudinal velocity in m/s,lateral velocity 
-...in m/s, and yaw angle rate in rad/s.
+% in m/s, and yaw angle rate in rad/s.
 
 A = [A1 zeros(2);
      zeros(2) A2]
